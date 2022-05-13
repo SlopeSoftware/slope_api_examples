934ce81d3fd5579e9919d6c288e92e1a26db53f9
Syed Hassaan Ali <hassaan.ali@slopesoftware.com>
2022-05-13 11:56:59 -0400

    Add example for C# .NET

---
+++ csharp/Slope.Api.Sample/Program.cs
@@ -0,0 +1,108 @@
+﻿using Slope.Api.Sample;
+
+const string rootPath = @"C:\Files\";
+
+// Substitute Real API credentials here
+const string apiKey = "<API KEY HERE>";
+const string apiSecret = "<API SECRET HERE>";
+
+const int modelId = 99999;  // The ID of the model to be run
+const int lookId = 999;  // The ID of the Look report to download
+const int templateId = 99999;  // The ID of the Projection Template to be run
+
+const int tableStructureId = 999999;  // The ID of the table structure of the data table to create
+const string dataTableFilePath = $"{rootPath}Assumption Update.xlsx";
+const string dataTableFileExcelSheetName = "Assumptions";
+const string dataTableName = "Data Table Name";
+
+const string modelPointFilePath = $"{rootPath}Inforce.csv";
+const string modelPointPortfolioName = "Portfolio 1";
+const string modelPointProductName = "Product A";
+
+const string scenarioTableFilePath = $"{rootPath}Scenario.xlsx";
+const string scenarioTableFileExcelSheetName = "Scenarios";
+var valuationDate = new DateTime(2021, 1, 31); // The start date of the scenario table
+
+const string reportDownloadFilePathExcel = $"{rootPath}Results.xlsx";
+const string reportDownloadFilePathCsv = $"{rootPath}Results.csv";
+
+
+var apiClient = await SlopeApi.CreateClientAsync(apiKey, apiSecret);
+
+
+Console.WriteLine("Uploading Data Table File");
+var dataTableFileId = await apiClient.UploadFileAsync(dataTableFilePath, $"Assumptions/Assumption Update {valuationDate:yyyy-MM}.xlsx");
+
+Console.WriteLine("Uploading Scenario File");
+var scenarioFileId = await apiClient.UploadFileAsync(scenarioTableFilePath, $"Scenario Files/Scenarios {valuationDate:yyyy-MM}.xlsx");
+
+Console.WriteLine("Uploading Inforce File");
+var modelPointFileId = await apiClient.UploadFileAsync(modelPointFilePath, $"Inforce/Inforce File - {valuationDate:yyyy-MM}.csv");
+
+
+Console.WriteLine("Creating Data Table");
+var dataTableId = await apiClient.CreateDataTableAsync(tableStructureId,
+    dataTableName,
+    fileId: dataTableFileId,
+    excelSheetName: dataTableFileExcelSheetName);
+
+Console.WriteLine("Creating Scenario Table");
+var scenarioTableId = await apiClient.CreateScenarioTableAsync(modelId, 
+    $"Scenarios - {valuationDate:yyyy-MM}",
+    valuationDate,
+    "BondEquivalent",
+    fileId: scenarioFileId,
+    excelSheetName: scenarioTableFileExcelSheetName);
+
+
+Console.WriteLine("Creating Projection from Template");
+var projectionId = await apiClient.CreateProjectionFromTemplateAsync(templateId, "API Test Projection");
+
+Console.WriteLine("Updating Projection Name and Scenario Table");
+await apiClient.UpdateProjectionAsync(projectionId, new SlopeApi.UpdateProjectionRequest("API Test Projection Rename", scenarioTableId));
+
+Console.WriteLine("Updating Projection Data Table");
+await apiClient.UpdateProjectionDataTableAsync(projectionId, new SlopeApi.UpdateProjectionDataTableAsyncRequest(new []
+{
+    new SlopeApi.UpdateProjectionTableDataTable(tableStructureId: tableStructureId, dataTableId: dataTableId)
+}));
+
+Console.WriteLine("Updating Projection Inforce File");
+await apiClient.UpdateProjectionModelPointFileAsync(projectionId, new SlopeApi.UpdateProjectionModelPointFileRequest(new []
+{
+    new SlopeApi.UpdateProjectionModelPointFilePortfolio(modelPointPortfolioName, new []
+    {
+        new SlopeApi.UpdateProjectionModelPointFileProduct(modelPointProductName, new SlopeApi.UpdateProjectionModelPointFileModelPointFile(modelPointFileId))
+    })
+}));
+
+
+Console.WriteLine("Starting Projection");
+await apiClient.RunProjectionAsync(projectionId);
+
+// Wait until the Projection is finished
+while (await apiClient.IsProjectionRunningAsync(projectionId))
+{
+    Console.WriteLine("Waiting for projection to finish...");
+    Thread.Sleep(15000); // Check once every 15 seconds if it is done
+}
+
+var status = await apiClient.GetProjectionStatusAsync(projectionId);
+Console.WriteLine($"Projection finished with status: {status}");
+
+
+// Download results only if the projection finished running
+if (status is "Completed" or "CompletedWithErrors")
+{
+    Console.WriteLine("Saving Look report in Excel format");
+    await apiClient.DownloadReportAsync(projectionId, lookId, reportDownloadFilePathExcel, "Excel", new Dictionary<string, string>
+    {
+        {"Time", "0"}
+    });
+
+    Console.WriteLine("Saving Look report in CSV format");
+    await apiClient.DownloadReportAsync(projectionId, lookId, reportDownloadFilePathCsv, "Csv", new Dictionary<string, string>
+    {
+        {"Time", "0"}
+    });
+}
---
+++ csharp/Slope.Api.Sample/SlopeApi.cs
@@ -0,0 +1,253 @@
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace Slope.Api.Sample;
+
+public class SlopeApi
+{
+    private const string ApiUrl = "https://api.slopesoftware.com";
+    private const string ApiVersion = "v1";
+
+    private readonly HttpClient _httpClient;
+
+    private SlopeApi()
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(ApiUrl)
+        };
+    }
+
+    public static async Task<SlopeApi> CreateClientAsync(string apiKey, string apiSecret)
+    {
+        var apiClient = new SlopeApi();
+        await apiClient.AuthorizeAsync(apiKey, apiSecret);
+
+        return apiClient;
+    }
+
+    private record AuthorizeRequest(string apiKey, string apiSecretKey);
+    private record AuthorizeResponse(string accessToken);
+    public async Task AuthorizeAsync(string apiKey, string apiSecretKey)
+    {
+        var loginRequest = new AuthorizeRequest(apiKey, apiSecretKey);
+        var response = await PostAsync<AuthorizeRequest, AuthorizeResponse>(loginRequest, $"/api/{ApiVersion}/Authorize");
+        
+        _httpClient.DefaultRequestHeaders.Accept.Clear();
+        _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", response.accessToken);
+    }
+    
+    private record GetUploadUrlRequest(string filePath);
+    private record GetUploadUrlResponse(string uploadUrl);
+    private record SaveUploadRequest(string filePath);
+    private record SaveUploadResponse(int fileId);
+    public async Task<int> UploadFileAsync(string filePath, string slopePath)
+    {
+        // Get a temporary direct upload url
+        var urlRequest = new GetUploadUrlRequest(slopePath);
+        var urlResponse = await PostAsync<GetUploadUrlRequest, GetUploadUrlResponse>(urlRequest, $"/api/{ApiVersion}/Files/GetUploadUrl");
+
+        // Upload file directly to S3
+        // Note: Do not use session here - this is a direct call to S3 and does not use the session auth
+        using var fileHttpClient = new HttpClient();
+        using var multipartFormContent = new MultipartFormDataContent();
+        var fileStreamContent = new StreamContent(File.OpenRead(filePath));
+        multipartFormContent.Add(fileStreamContent);
+        
+        // Set content type to null to prevent S3 from validating the file type
+        multipartFormContent.Headers.ContentType = null;
+        
+        var fileResponse = await fileHttpClient.PutAsync(urlResponse.uploadUrl, multipartFormContent);
+        await CheckResponseAsync(fileResponse);
+
+        // Tell SLOPE we are done uploading
+        var saveRequest = new SaveUploadRequest(slopePath);
+        var saveResponse = await PostAsync<SaveUploadRequest, SaveUploadResponse>(saveRequest, $"/api/{ApiVersion}/Files/SaveUpload");
+        return saveResponse.fileId;
+    }
+    
+    private record CreateDataTableRequest(int tableStructureId,
+        string name,
+        int? fileId,
+        string? filePath,
+        string? excelSheetName,
+        string? delimiter);
+    private record CreateDataTableResponse(int id);
+    public async Task<int> CreateDataTableAsync(int tableStructureId,
+        string name,
+        int? fileId = null,
+        string? slopePath = null,
+        string? excelSheetName = null,
+        string delimiter = ",")
+    {
+        var request = new CreateDataTableRequest(tableStructureId, name, fileId, slopePath, excelSheetName, delimiter);
+        var response = await PostAsync<CreateDataTableRequest, CreateDataTableResponse>(request, $"/api/{ApiVersion}/DataTables");
+        return response.id;
+    }
+    
+    private record CreateScenarioTableRequest(int modelId,
+        string name,
+        string startDate,
+        string yieldCurveRateType,
+        int? fileId,
+        string? filePath,
+        string? excelSheetName,
+        string? delimiter);
+    private record CreateScenarioTableResponse(int id);
+    public async Task<int> CreateScenarioTableAsync(int modelId,
+        string name,
+        DateTime startDate,
+        string yieldCurveRateType,
+        int? fileId = null,
+        string? slopePath = null,
+        string? excelSheetName = null,
+        string delimiter = ",")
+    {
+        var request = new CreateScenarioTableRequest(modelId,
+            name,
+            startDate.Date.ToString("o"),
+            yieldCurveRateType,
+            fileId,
+            slopePath,
+            excelSheetName,
+            delimiter);
+        
+        var response = await PostAsync<CreateScenarioTableRequest, CreateScenarioTableResponse>(request, $"/api/{ApiVersion}/ScenarioTables");
+        return response.id;
+    }
+
+    private record CreateProjectionRequest(int templateId, string name);
+    private record CreateProjectionResponse(int id);
+    public async Task<int> CreateProjectionFromTemplateAsync(int templateId, string name)
+    {
+        var request = new CreateProjectionRequest(templateId, name);
+        var response = await PostAsync<CreateProjectionRequest, CreateProjectionResponse>(request, $"/api/{ApiVersion}/Projections");
+        return response.id;
+    }
+
+    public record UpdateProjectionRequest(string? name = null, int? scenarioTableId = null);
+    public async Task UpdateProjectionAsync(int projectionId, UpdateProjectionRequest request)
+    {
+        await PatchAsync(request, $"/api/{ApiVersion}/Projections/{projectionId}");
+    }
+    
+    public record UpdateProjectionDataTableAsyncRequest(UpdateProjectionTableDataTable[] dataTables);
+    public record UpdateProjectionTableDataTable(int? tableStructureId = null,
+        string? tableStructureName= null,
+        int? dataTableId= null,
+        string? dataTableName= null,
+        int? dataTableVersion= null);
+    public async Task UpdateProjectionDataTableAsync(int projectionId, UpdateProjectionDataTableAsyncRequest request)
+    {
+        await PatchAsync(request, $"/api/{ApiVersion}/Projections/{projectionId}");
+    }
+    
+    public record UpdateProjectionModelPointFileRequest(UpdateProjectionModelPointFilePortfolio[] portfolios);
+    public record UpdateProjectionModelPointFilePortfolio(string portfolioName, UpdateProjectionModelPointFileProduct[] products);
+    public record UpdateProjectionModelPointFileProduct(string productName, UpdateProjectionModelPointFileModelPointFile modelPointFile);
+    public record UpdateProjectionModelPointFileModelPointFile(long fileId);
+    public async Task UpdateProjectionModelPointFileAsync(int projectionId, UpdateProjectionModelPointFileRequest request)
+    {
+        await PatchAsync(request, $"/api/{ApiVersion}/Projections/{projectionId}");
+    }
+
+    public async Task RunProjectionAsync(int projectionId)
+    {
+        var content = new StringContent(string.Empty, Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync($"/api/{ApiVersion}/Projections/{projectionId}/run", content);
+        if (!response.IsSuccessStatusCode)
+        {
+            var errorMessage = $"Failed to start projection with id: {projectionId}\r\n{await response.Content.ReadAsStringAsync()}";
+            throw new Exception(errorMessage);
+        }
+    }
+
+    private record IsProjectionRunningResponse(bool isRunning);
+    public async Task<bool> IsProjectionRunningAsync(int projectionId)
+    {
+        var response = await TryGetAsync<IsProjectionRunningResponse>($"/api/{ApiVersion}/Projections/{projectionId}");
+        if (response == null)
+        {
+            return false;
+        }
+        
+        return response.isRunning;
+    }
+
+    private record GetProjectionStatusResponse(string status);
+    public async Task<string> GetProjectionStatusAsync(int projectionId)
+    { 
+        var response = await GetAsync<GetProjectionStatusResponse>($"/api/{ApiVersion}/Projections/{projectionId}");
+        return response.status;
+    }
+
+    public record DownloadReportRequest(string reportFormat, Dictionary<string, string> filters);
+    public async Task DownloadReportAsync(int projectionId, int lookId, string fileName, string reportFormat, Dictionary<string, string>? filters = null)
+    {
+        var finalFilters = new Dictionary<string, string>(filters ?? new Dictionary<string, string>());
+        finalFilters.TryAdd("Projection ID", projectionId.ToString());
+        
+        var request = new DownloadReportRequest(reportFormat, finalFilters);
+        var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync($"/api/{ApiVersion}/Reports/Looks/{lookId}", content);
+        await CheckResponseAsync(response);
+
+        await using (var file = File.OpenWrite(fileName))
+        await using (var report = await response.Content.ReadAsStreamAsync())
+        {
+            await report.CopyToAsync(file);
+        }
+    }
+
+    private async Task CheckResponseAsync(HttpResponseMessage response)
+    {
+        if (!response.IsSuccessStatusCode)
+        {
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+        response.EnsureSuccessStatusCode();
+    }
+
+    private async Task<TResponse> GetAsync<TResponse>(string url)
+    {
+        var response = await _httpClient.GetAsync(url);
+        await CheckResponseAsync(response);
+
+        var responseJson = await response.Content.ReadAsStringAsync();
+        return DeserializeJsonOrThrow<TResponse>(responseJson, url);
+    }
+    
+    private async Task<TResponse?> TryGetAsync<TResponse>(string url) where TResponse : class
+    {
+        var response = await _httpClient.GetAsync(url);
+        if (!response.IsSuccessStatusCode)
+        {
+            return null;
+        }
+
+        var responseJson = await response.Content.ReadAsStringAsync();
+        return DeserializeJsonOrThrow<TResponse>(responseJson, url);
+    }
+
+    private async Task<TResponse> PostAsync<TRequest, TResponse>(TRequest request, string url)
+    {
+        var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+        var response = await _httpClient.PostAsync(url, content);
+        await CheckResponseAsync(response);
+
+        var responseJson = await response.Content.ReadAsStringAsync();
+        return DeserializeJsonOrThrow<TResponse>(responseJson, url);
+    }
+
+    private async Task PatchAsync<TRequest>(TRequest request, string url)
+    {
+        var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+        var response = await _httpClient.PatchAsync(url, content);
+        await CheckResponseAsync(response);
+    }
+    
+    private TResponse DeserializeJsonOrThrow<TResponse>(string json, string url) =>
+        JsonSerializer.Deserialize<TResponse>(json) ?? throw new Exception("Invalid JSON from endpoint: " + url);
+}
